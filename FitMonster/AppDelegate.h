b7607eebc9464d4737081596fb676719a31e80fd
Drew Dulle <dulleaj@gmail.com>
2016-07-13 19:36:32 -0400

    First commit

---
+++ FitMonster/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FitMonster
+//
+//  Created by Andrew Dulle on 7/13/16.
+//  Copyright © 2016 Andrew Dulle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FitMonster/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FitMonster
+//
+//  Created by Andrew Dulle on 7/13/16.
+//  Copyright © 2016 Andrew Dulle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
